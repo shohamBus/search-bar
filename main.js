8768277782aa756f80c9e2e9db28c9222e33d8f9
shohamBus <shohambuskila@gmail.com>
2022-05-25 15:36:24 +0300

    search bar

---
+++ main.js
@@ -0,0 +1,26 @@
+const search = document.querySelector(".search-bar");
+const list = document.querySelector(".list");
+
+let array = ["apple", "banana", "orange",];
+display(array);
+function display(array) {
+    list.innerHTML = "";
+    if(array.length===0){
+    newLi = document.createElement("li");
+    newLi.textContent = "no result found!";
+    list.append(newLi);
+    newLi.classList.add("no-result-found")
+    }
+  for (const item of array) {
+    newLi = document.createElement("li");
+    newLi.textContent = item;
+    list.append(newLi);
+  }
+}
+
+search.oninput = function () {
+    const filterArr = array.filter(function (v) {
+        return v.includes(search.value);
+    });
+    display(filterArr);
+}
